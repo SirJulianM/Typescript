7494a68e9542d8bf7c549650ed6bd683ee26f546
SirJulianM <ing_julianmadrid@hotmail.com>
2022-12-26 15:16:11 -0500

    Opcion include y exclude

---
+++ Clase3/app/build/app.js
@@ -0,0 +1,6 @@
+console.log("Hola desde el archivo app");
+var lista = ["Julián", "Mónica", "Freddy", "Sofía"];
+for (var _i = 0, lista_1 = lista; _i < lista_1.length; _i++) {
+    var iterator = lista_1[_i];
+    console.log(iterator);
+}
