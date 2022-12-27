e87ef633cbda60851e4be69d06849b0e551675d5
SirJulianM <ing_julianmadrid@hotmail.com>
2022-12-27 18:24:54 -0500

    Fourteenth class of Typescript

---
+++ Clase13/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var numero = 120;
+var numero = 23;
+let cadena = "Hola";
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+}
+//console.log(index);
