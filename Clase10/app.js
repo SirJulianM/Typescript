ab91d12c19724fb40086aa2f24381e7f9f196379
SirJulianM <ing_julianmadrid@hotmail.com>
2022-12-27 10:52:32 -0500

    Tenth class of Typescript

---
+++ Clase10/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let cualquierValor;
+cualquierValor = "Mónik is Queen";
+console.log(cualquierValor.length);
+cualquierValor = false;
+console.log(cualquierValor);
+cualquierValor = 12;
+console.log(typeof cualquierValor);
