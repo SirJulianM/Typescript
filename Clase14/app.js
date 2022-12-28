433b74356793fd1dfe5a53cd75c1c479645cf54b
SirJulianM <ing_julianmadrid@hotmail.com>
2022-12-28 07:38:48 -0500

    Fourteen class of Typescript

---
+++ Clase14/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const ESTADO = false;
+//ESTADO = true;
+if (true) {
+    const ESTADO = true;
+}
+for (const iterator in [1, 2, 3, 4, 5, 6, 7]) {
+    console.log(iterator);
+}
---
+++ Clase14/app.ts
@@ -0,0 +1,10 @@
+const ESTADO:Boolean = false;
+//ESTADO = true;
+
+if(true){
+    const ESTADO = true;
+}
+
+for (const iterator in [1,2,3,4,5,6,7]) {
+    console.log(iterator);
+}
