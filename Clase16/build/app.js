a3c1d097565f0ab0bc4f26a5426bcf4333441d1b
SirJulianM <ing_julianmadrid@hotmail.com>
2023-01-19 17:53:30 -0500

    Sixteen class of typescript

---
+++ Clase16/build/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let persona;
+persona = [["Julián", "Madrid"], 35, "masculino", ["programador", "piano"]];
+//let [nombre, edad, genero, intereses] = persona;
+let [, , , intereses] = persona;
+//console.log(nombre);
+//console.log(edad);
+console.log(intereses);
